2d806168a0603b6263f0bc7731d3670d7a67a042
David Sadler <davidwallacesadler@gmail.com>
2021-01-01 11:50:58 -0700

    Add profile image header

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,33 @@
 <template>
-  <div class="py-4 d-flex flex-column align-items-center">
-    <div class="p-5">
+  <div class="d-flex flex-column align-items-center">
+
+    <div class="profile-header position-relative d-flex align-items-center justify-content-center">
+      <div class="background-rect position-absolute" />
+      <b-img class="profile-image" src="david-sadler.png" rounded="circle" v-bind="profileImgProps" />
+    </div>
+
+    <div class="p-5 folio-content-max-width">
       <h1 class="folio-weight-medium">
         Hi I'm 
-        <span class="text-active folio-font-large folio-weight-thick">David,</span>
+        <span class="text-active folio-font-large folio-weight-thick">David</span>
       </h1>
-      <h1 class="folio-font-medium folio-weight-medium">A Driven Designer, Web and Mobile Developer.</h1>
-      <h2 class="folio-weight-medium">And I love to work with:</h2>
+      <h1 class="folio-font-medium folio-weight-medium">A Driven Designer, Web and Mobile Developer</h1>
+      <h2 class="folio-font-medium folio-weight-medium">I love to work with:</h2>
+      <div class="d-flex row-wrap">
+        <div v-for="(logo,i) in techLogos" :key="i" style="flex-basis:50px;">
+          <b-img class="tech-image m-1" :src="logo.asset" :alt="logo.alt" v-b-tooltip.hover :title="logo.tip" />
+        </div>
+      </div>
     </div>
-    <div class="px-5 py-3 d-flex border-bottom-theme">
-      <img class="tech-image m-1" v-for="(logo,i) in techLogos" :key="i" :src="logo" :alt="logo">
-    </div> 
+
+      
+    
+    <div>
+      <div class="texture-background">
+        
+      </div>
+    </div>
+  
     <!-- <ContentAreaHeader text="Work" />
     <ProjectSectionHeader text="Web Projects" />
     <ProjectCardList :card-data="webProjects" />
@@ -24,16 +41,17 @@
 export default {
   data() {
     return {
-      techLogos: ["html5-logo.png",
-                  "css3-logo.png",
-                  "javaScript-logo.png",
-                  "vue-logo.png",
-                  "swift-logo.png",
-                  "kotlin-logo.png",
-                  "xd-logo.png",
-                  "ai-logo.png",
-                  "ps-logo.png",
-                  "blender-logo.png"
+      profileImgProps: { width: 250, height: 250, fluid: true, },
+      techLogos: [{ asset:"html5-logo.png", tip: "HTML5", alt: "Html 5 logo"},
+                  { asset:"css3-logo.png", tip: "CSS3", alt: "Css 5 logo"},
+                  { asset: "javaScript-logo.png", tip: "JavaScript", alt: "java script logo"},
+                  { asset: "vue-logo.png", tip: "Vue", alt: "Vue logo"},
+                  { asset: "swift-logo.png", tip: "Swift", alt: "Swift logo"},
+                  { asset: "kotlin-logo.png", tip: "Kotlin", alt: "Kotlin logo"},
+                  { asset: "xd-logo.png", tip: "Adobe Xd", alt: "Adobe x.d logo"},
+                  { asset: "ai-logo.png", tip: "Adobe Illustrator", alt: "Adobe Illustrator logo"},
+                  { asset: "ps-logo.png", tip: "Adobe Photoshop", alt: "Adobe Photoshop logo"},
+                  { asset: "blender-logo.png", tip: "Blender", alt: "Blender logo"}
                   ],
       isLeopoldOpen: false,
       isBpOpen: false,
@@ -63,10 +81,28 @@ export default {
 
 <style lang="scss">
 @import '../assets/styles/main.scss';
-
+.overflow-hidden {
+  overflow: hidden;
+}
+.profile-header {
+  width: 100vw;
+  height: 260px;
+}
+.profile-image {
+  z-index: 2;
+}
+.background-rect {
+  z-index: 1;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 200px;
+  background-color: $folio-theme-color;
+}
 .tech-image {
   max-width: 50px;
   max-height: 50px;
+  transition: all 0.25s;
 }
 
 .hover-card {
