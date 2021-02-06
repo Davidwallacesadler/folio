9584c57bf5bf79cb560f6e53982f8e5ff0742054
David Sadler <davidwallacesadler@gmail.com>
2021-02-05 17:05:09 -0700

    Tring differnent deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,6 +5,9 @@ export default {
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
 
+  router: {
+    base: '/folio/'
+  },
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: 'folio',
